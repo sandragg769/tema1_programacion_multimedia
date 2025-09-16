fda904b1feb2fec2e867102a0d7f9f8ba156d45b
Sandra Gómez García <sandragm67@gmail.com>
2025-09-16 18:10:00 +0200

    ejercicios foreach y filter

--- app/src/main/java/com/example/tema11/ejercicio5.kt
+++ app/src/main/java/com/example/tema11/ejercicio5.kt
@@ -1,2 +1,10 @@
 package com.example.tema11
 
+fun main() {
+    var listaNumeros = listOf(-1, 5, -8, 3, 10)
+    println(filtrarParesPositivos(listaNumeros))
+}
+
+fun filtrarParesPositivos(lista: List<Int>): List<Int> {
+    return lista.filter { it >= 0 && it % 2 != 0 }
+}
